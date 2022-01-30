30bba769a132380ab3aacfe337c7cf77bf5ff7f1
shivshankar31 <shivshankar31@yahoo.com>
2022-01-31 04:46:22 +0530

    17: 18, 19, 20:  view created, add urls.py

--- rest_fristproj/firstapp/views.py
+++ rest_fristproj/firstapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def employeeView(request):
+    emp = {
+        'ID' : 1,
+        'Name' : 'Joann',
+        'location' : 'London',
+        'Salary' : 1000000
+    }
+
+    return JsonResponse(emp)
