2a483e089aa12909459cd9c532ba93a0da6bab62
shivshankar31 <shivshankar31@yahoo.com>
2022-01-31 07:44:02 +0530

    21, 22,: step 3: create models, change database

--- rest_fristproj/firstapp/models.py
+++ rest_fristproj/firstapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class empModel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.id + self.name + self.location + self.salary
--- rest_fristproj/rest_fristproj/settings.py
+++ rest_fristproj/rest_fristproj/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'rest_fristproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'employeedb',
+        'USER' : 'root',
+        'PASSWORD': ''
     }
 }
 
