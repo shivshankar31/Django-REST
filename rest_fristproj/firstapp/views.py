b1729f7b4b7a9d9b7d3f9d6967b2edbe5e8d12f9
shivshankar31 <shivshankar31@yahoo.com>
2022-02-01 05:34:42 +0530

    23: step 4: call data from database

--- rest_fristproj/firstapp/views.py
+++ rest_fristproj/firstapp/views.py
@@ -1,13 +1,21 @@
+from urllib import response
 from django.shortcuts import render
 from django.http import JsonResponse
+from firstapp.models import empModel
+
 # Create your views here.
 
 def employeeView(request):
-    emp = {
-        'ID' : 1,
-        'Name' : 'Joann',
-        'location' : 'London',
-        'Salary' : 1000000
+    # emp = {
+    #     'ID' : 1,
+    #     'Name' : 'Joann',
+    #     'location' : 'London',
+    #     'Salary' : 1000000
+    # }
+
+    data = empModel.objects.all();
+    response = {
+        'employees' : list(data.values('id', 'name', 'location', 'salary'))
     }
 
-    return JsonResponse(emp)
+    return JsonResponse(response)
